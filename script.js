e3b5f3cd690f9675a66563a436375bc189319b10
Guilherme Salvador <guilherme.s.2@outlook.com>
2024-02-19 10:47:04 -0300

    refactor with my infos

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function toggleMode() {
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  // substituir a imagem
-  if (html.classList.contains('light')) {
-    // se estiver no light mode, adicionar a imagem light
-    img.setAttribute('src', './assets/assets/avatar-light.png')
-    img.setAttribute('alt', 'Imagem do Mayk Brito de óculos escuro')
-  } else {
-    // se estiver sem light mode, manter a imagem original
-    img.setAttribute('src', './assets/assets/avatar.png')
-    img.setAttribute('alt', 'Imagem do Mayk Brito')
-  }
+  // // substituir a imagem
+  // if (html.classList.contains('light')) {
+  //   // se estiver no light mode, adicionar a imagem light
+  //   img.setAttribute('src', './assets/assets/avatar-light.png')
+  //   img.setAttribute('alt', 'Imagem do Mayk Brito de óculos escuro')
+  // } else {
+  //   // se estiver sem light mode, manter a imagem original
+  //   img.setAttribute('src', './assets/assets/avatar.png')
+  //   img.setAttribute('alt', 'Imagem do Mayk Brito')
+  // }
 }
